fb20c2406b36f311447a028aa174a82f74b3051b
Teesha Karotra <teeshakarotra@gmail.com>
2022-02-21 16:39:07 +0530

    Equip param

--- nivdas/static/js/pages/table-editable.int.js
+++ nivdas/static/js/pages/table-editable.int.js
@@ -1,1 +1,38 @@
-$(function(){var e={};$(".table-edits tr").editable({dropdowns:{eqp_status:["Active","InActive"]},edit:function(t){$(".edit i",this).removeClass("fa-pencil-alt").addClass("fa-save").attr("title","Save")},save:function(t){$(".edit i",this).removeClass("fa-save").addClass("fa-pencil-alt").attr("title","Edit"),this in e&&(e[this].destroy(),delete e[this])},cancel:function(t){$(".edit i",this).removeClass("fa-save").addClass("fa-pencil-alt").attr("title","Edit"),this in e&&(e[this].destroy(),delete e[this])}})});
+$(function(){
+    var e={};
+    $(".table-edits tr").editable({
+        dropdowns:{eqp_status:["Active","InActive"]},
+        edit:function(t){
+            $(".edit i",this).removeClass("fa-pencil-alt").addClass("fa-save").attr("title","Save")
+        },
+        save:function(t){
+            $('#modal').modal('show');
+            $(".edit i",this).removeClass("fa-save").addClass("fa-pencil-alt").attr("title","Edit"),
+            this in e&&(e[this].destroy(),delete e[this])
+        },
+        cancel:function(t){
+            $(".edit i",this).removeClass("fa-save").addClass("fa-pencil-alt").attr("title","Edit"),
+            this in e&&(e[this].destroy(),delete e[this])
+        }
+    })
+});
+
+
+// $(function(){
+//     var e={};
+//     $(".table-edits tr").editable({
+//         dropdowns:{eqp_status:["Active","InActive"]},
+//         edit:function(t){
+//             $(".edit i",this).removeClass("fa-pencil-alt").addClass("fa-save").attr("title","Save")
+//         },
+//         save:function(t){
+//             $('#modal').modal('show');
+//             $(".edit i",this).removeClass("fa-save").addClass("fa-pencil-alt").attr("title","Edit"),
+//             this in e&&(e[this].destroy(),delete e[this])
+//         },
+//         cancel:function(t){
+//             $(".edit i",this).removeClass("fa-save").addClass("fa-pencil-alt").attr("title","Edit"),
+//             this in e&&(e[this].destroy(),delete e[this])
+//         }
+//     })
+// });
